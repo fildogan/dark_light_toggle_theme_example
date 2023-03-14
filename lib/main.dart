52d2ea48000958f7b9e155a2c94e785590879878
filip doganowski <filip.doganowski@gmail.com>
2023-03-14 20:03:53 +0100

    Update MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dark_light_toggle_theme_example/config.dart';
 import 'package:dark_light_toggle_theme_example/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -5,13 +6,28 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    currentTheme.addListener(() {
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      themeMode: currentTheme.currentTheme,
       home: const HomePage(),
     );
   }
